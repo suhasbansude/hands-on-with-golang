75463a8a989720f9957e265ae7f842f24b2b0c85
Suhas Bansude <suhasb@mkclindia.local>
2019-07-04 17:09:03 +0530

    'iota'

---
+++ 02-Variables & Values & Types/02-iota.go
@@ -0,0 +1,45 @@
+package main
+
+import "fmt"
+
+const (
+	A1 = iota
+	A2
+	A3
+)
+
+const (
+	B1 = iota
+	B2
+	B3
+)
+
+const (
+	C1 = iota
+	C2 = iota
+	C3 = iota
+)
+
+const (
+	D1 = iota
+	D2
+	_
+	D3
+)
+
+const (
+	E1 = iota + 2 // Strat at 2
+	E2            // Increment by 1
+	E3
+)
+
+func main() {
+	fmt.Println(A1, A2, A3)
+	fmt.Println(B1, B2, B3)
+	fmt.Println(C1, C2, C3)
+	// Skip value
+	fmt.Println(D1, D2, D3)
+
+	// Incremental Value
+	fmt.Println(E1, E2, E3)
+}
